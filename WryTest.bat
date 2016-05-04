4d637df66ad4c90570e635f318cfb4b94fa37480
Nathanael <nathanael.j.duke@gmail.com>
2016-05-03 23:43:53 -0400

    Generalization
    Make log path dynamic (based on source file path).

--- WryTest.bat
+++ WryTest.bat
@@ -2,15 +2,16 @@
 :ask
 
 echo Target path to test [C:\],[D:\Temp\],[\\share\path\]
-echo with trailing slash (C:\Temp\)
+echo **With trailing slash (C:\Temp\)**
 set /p targetPath=
 echo.
 
-echo Name of the file
+echo Name of the file (preferably a large one)
 set /p fileName=
 echo.
 
 echo Full path to the source folder (with trailing slash):
+echo (also the log location)
 set /p sourcePath=
 echo.
 
@@ -35,17 +36,20 @@ IF /I "%confirm%"=="n" goto ask
 
 :proceed
 cls
+echo.
+echo.
 set /p paws=Press any key to continue or CTRL+C to cancel...
 echo It begins...
 
 :looper
 @echo on
 IF %counter% LEQ 0 goto END
-echo %time% >>log.txt
-xcopy /F %sourcePath%%fileName% %targetPath% >>log.txt
-del %targetPath%%fileName% >>log.txt
-echo %counter% iterations remaining >>log.txt
+echo %time% >>"%sourcePath%"log.txt
+xcopy /F /Y "%sourcePath%%fileName%" "%targetPath%" >>"%sourcePath%"log.txt
+del "%targetPath%%fileName%" >>"%sourcePath%"log.txt
 set /A counter = %counter% - 1
+echo %counter% iterations remaining >>"%sourcePath%"log.txt
+echo.
 goto looper
 :END
-exit >>log.txt
+exit >>"%sourcePath%"log.txt
